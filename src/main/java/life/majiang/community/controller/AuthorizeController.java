bc8de92062282d8dae6ea24c2da75e0c61bac8d8
Tianshuang <1546146802@qq.com>
2019-11-10 19:45:33 +0800

    add info

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,48 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.Model.User;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Autowired
+    private life.majiang.community.mapper.UserMapper userMapper;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("01ae392e635fcc518ecf");
+        accessTokenDTO.setClient_secret("30bbc2db0b2233ccbcab350e19d3dc905009a211");
+        String accessToken= githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser =githubProvider.getUser(accessToken);
+        if(githubUser !=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
+
+    }
+}
